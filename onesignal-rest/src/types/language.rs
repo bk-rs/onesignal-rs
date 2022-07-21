2a274f7418d92676b6dc64ddfc7e9f7b08d4b085
vkill <vkill.net@gmail.com>
2022-07-21 14:33:21 +0800

    Update

--- onesignal-rest/src/types/language.rs
+++ onesignal-rest/src/types/language.rs
@@ -1,10 +1,49 @@
-use serde::{Deserialize, Serialize};
+// https://documentation.onesignal.com/docs/language-localization#what-languages-are-supported
+
+use serde_enum_str::{Deserialize_enum_str, Serialize_enum_str};
 
 //
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Deserialize_enum_str, Serialize_enum_str, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Language {
     #[serde(rename = "en")]
     En,
     #[serde(rename = "zh-Hans")]
     ZhHans,
+    #[serde(other)]
+    Other(Box<str>),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ser() {
+        #[derive(serde::Serialize)]
+        struct Foo {
+            language: Language,
+        }
+
+        assert_eq!(
+            serde_json::to_string(&Foo {
+                language: Language::En
+            })
+            .unwrap(),
+            r#"{"language":"en"}"#
+        );
+        assert_eq!(
+            serde_json::to_string(&Foo {
+                language: Language::ZhHans
+            })
+            .unwrap(),
+            r#"{"language":"zh-Hans"}"#
+        );
+        assert_eq!(
+            serde_json::to_string(&Foo {
+                language: Language::Other("vi".into())
+            })
+            .unwrap(),
+            r#"{"language":"vi"}"#
+        );
+    }
 }
